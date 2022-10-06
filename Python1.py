bc037eae5cb8e723270c496f45a71084471bd198
Amit-er <67402683+Amit-er@users.noreply.github.com>
2022-10-06 20:25:55 +0530

    Create Python1.py

---
+++ Python1.py
@@ -0,0 +1,9 @@
+num = int(input("Enter any Number -  "))
+while(num>0):
+    j=num%10
+    if j!=0 and j!=1:
+        print("Number is not binary")
+        break
+    num=//10
+    if ==0:
+        print("Number is binary") 
